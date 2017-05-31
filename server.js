4a2a943e82865a68af53167857a3703071e8f2c3
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-05-31 10:25:56 +0200

    server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('May Node be with you')
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+  response.send('Hello World')
+})
+
+
+app.listen(3000, function(){
+  console.log('listening on 3000')
+})
